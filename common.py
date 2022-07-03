966a3c95b8332c1d7fdbde9b8952ef67847be117
dunaiev <hecksr3@gmail.com>
2022-07-03 17:13:35 +0300

    generate matrix

--- common.py
+++ common.py
@@ -15,3 +15,16 @@ def pp(matrix, space=3, divider=",", border="|", end=True):
         print(border + ss + border)
     if end:
         print('')
+
+
+def make_matrix(type, **options):
+    R = options.get('R', 4)
+    if type == "1":
+        return [[1 for _ in range(R)] for _ in range(R)]
+    if type == "rand":
+        Min = options.get('Min', 1)
+        Max = options.get('Max', 10)
+        return [[random.randint(Min, Max) for _ in range(R)] for _ in range(R)]
+    if type == "up":
+        return [[1+x+y*R for x in range(R)] for y in range(R)]
+    raise Exception("setting type is required")
